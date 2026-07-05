45c00b9e004a0fdc08dd141d9473733858d7d47d
CrystaltheMonkey <mr.leon.12.viv@gmail.com>
2026-07-05 13:25:44 -0700

    Add VRChat SDK packages

---
+++ Packages/com.vrchat.base/Editor/VRCSDK/Dependencies/VRChat/VRCCopyrightAgreement.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VRC.Core;
+using VRC.SDKBase.Editor;
+using VRC.SDKBase.Editor.Api;
+using VRC.SDKBase.Editor.Elements;
+
+[assembly: InternalsVisibleTo("VRC.SDK3A.Editor")]
+[assembly: InternalsVisibleTo("VRC.SDK3.Editor")]
+namespace VRC.SDKBase
+{
+    public static class VRCCopyrightAgreement
+    {
+        private const int Version = 1;
+        private const string AgreementCode = "content.copyright.owned";
+
+        public static string AgreementText =>
+            "By clicking OK, I certify that I have the necessary rights to upload this content and that it will not infringe on any third-party legal or intellectual property rights.";
+        
+        private const string SESSION_STATE_PREFIX = "VRCSdkControlPanel";
+        private const string AGREEMENT_KEY = SESSION_STATE_PREFIX + ".CopyrightAgreement";
+        private const string CONTENT_LIST_KEY = AGREEMENT_KEY + ".ContentList";
+
+        private static List<string> AgreedContentThisSession
+        {
+            get
+            {
+                var saved = SessionState.GetString(CONTENT_LIST_KEY, null);
+                if (string.IsNullOrWhiteSpace(saved))
+                {
+                    return new List<string>();
+                }
+                
+                return saved.Split(';').ToList();
+            }
+        }
+        
+        private static void SaveContentAgreementToSession(string contentId)
+        {
+            var agreed = AgreedContentThisSession;
+            agreed.Add(contentId);
+            SessionState.SetString(CONTENT_LIST_KEY, string.Join(";", agreed));
+        }
+
+        internal static async Task<bool> HasAgreement(string contentId)
+        {
+            var hasSessionAgreement = AgreedContentThisSession.Contains(contentId);
+            try
+            {
+                var result = await VRCApi.CheckContentUploadConsent(new VRCAgreementCheckRequest
+                {
+                    AgreementCode = AgreementCode,
+                    ContentId = contentId,
+                    Version = Version
+                });
+
+                // Force to re-agree if not saved in session storage
+                return result.Agreed && hasSessionAgreement;
+            }
+            catch (Exception e)
+            {
+                Core.Logger.LogException(e);
+            }
+            return false;
+        }
+
+        internal static async Task<bool> Agree(string contentId)
+        {
+            try
+            {
+                var result = await VRCApi.ContentUploadConsent(new VRCAgreement
+                {
+                    AgreementCode = AgreementCode,
+                    AgreementFulltext = AgreementText,
+                    ContentId = contentId,
+                    Version = Version
+                });
+                var agreed = result.ContentId == contentId && result is {Version: Version, AgreementCode: AgreementCode};
+                if (!agreed) return false;
+                
+                SaveContentAgreementToSession(contentId);
+
+                return true;
+            }catch (ApiErrorException e)
+            {
+                Core.Logger.LogError(e.ErrorMessage);    
+            } catch (Exception e)
+            {
+                Core.Logger.LogException(e);
+            }
+            return false;
+        }
+        
+        /// <summary>
+        /// Check if the user has agreed to the copyright agreement.
+        /// This should happen once per session per content ID
+        /// </summary>
+        /// <param name="pipelineManager"></param>
+        /// <param name="content"></param>
+        /// <exception cref="Exception"></exception>
+        /// <exception cref="OwnershipException"></exception>
+        [PublicAPI]
+        public static async Task CheckCopyrightAgreement(PipelineManager pipelineManager, IVRCContent content)
+        {
+            // ---- PATCHED: Always agree silently ----
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Packages/com.vrchat.base/Runtime/VRCSDK/Dependencies/VRChat/Scripts/Validation/PropValidation.cs
@@ -0,0 +1,106 @@
+﻿#if VRC_ENABLE_PROPS
+
+namespace VRC.SDKBase.Validation
+{
+    // ReSharper disable once PartialTypeWithSinglePart
+    public static partial class PropValidation
+    {
+        public static readonly string[] ComponentTypeWhiteListCommon = {
+            "UnityEngine.Light",
+            "UnityEngine.BoxCollider",
+            "UnityEngine.SphereCollider",
+            "UnityEngine.CapsuleCollider",
+            "UnityEngine.MeshCollider",
+            "UnityEngine.Rigidbody",
+            "UnityEngine.Joint",
+            "UnityEngine.AudioSource",
+            #if !VRC_CLIENT
+            "VRC.Core.PipelineSaver",
+            #endif
+            "VRC.Core.PipelineManager",
+            "UnityEngine.Transform",
+            "UnityEngine.Animator",
+            "UnityEngine.SkinnedMeshRenderer",
+            "UnityEngine.MeshFilter",
+            "UnityEngine.MeshRenderer",
+            "UnityEngine.ParticleSystem",
+            "UnityEngine.ParticleSystemRenderer",
+            "UnityEngine.TrailRenderer",
+            "UnityEngine.LineRenderer",
+            #region UI Items
+            "UnityEngine.CanvasRenderer",
+            "UnityEngine.Canvas",
+            "UnityEngine.UI.CanvasScaler",
+            "TMPro.TextMeshProUGUI",
+            "TMPro.TMP_Dropdown",
+            "TMPro.TMP_InputField",
+            "TMPro.TMP_ScrollbarEventHandler",
+            "TMPro.TMP_SelectionCaret",
+            "TMPro.TMP_SpriteAnimator",
+            "TMPro.TMP_SubMesh",
+            "TMPro.TMP_SubMeshUI",
+            "TMPro.TMP_Text",
+            "TMPro.TextMeshPro",
+            "TMPro.TextMeshProUGUI",
+            "TMPro.TextContainer",
+            "TMPro.TMP_Dropdown+DropdownItem",
+            "UnityEngine.UI.Button",
+            "UnityEngine.UI.Dropdown",
+            "UnityEngine.UI.Dropdown+DropdownItem",
+            "UnityEngine.UI.Graphic",
+            "UnityEngine.UI.GraphicRaycaster",
+            "UnityEngine.UI.Image",
+            "UnityEngine.UI.InputField",
+            "UnityEngine.UI.Mask",
+            "UnityEngine.UI.MaskableGraphic",
+            "UnityEngine.UI.RawImage",
+            "UnityEngine.UI.RectMask2D",
+            "UnityEngine.UI.Scrollbar",
+            "UnityEngine.UI.ScrollRect",
+            "UnityEngine.UI.Selectable",
+            "UnityEngine.UI.Slider",
+            "UnityEngine.UI.Text",
+            "UnityEngine.UI.Toggle",
+            "UnityEngine.UI.ToggleGroup",
+            "UnityEngine.UI.AspectRatioFitter",
+            "UnityEngine.UI.CanvasScaler",
+            "UnityEngine.UI.ContentSizeFitter",
+            "UnityEngine.UI.GridLayoutGroup",
+            "UnityEngine.UI.HorizontalLayoutGroup",
+            "UnityEngine.UI.HorizontalOrVerticalLayoutGroup",
+            "UnityEngine.UI.LayoutElement",
+            "UnityEngine.UI.LayoutGroup",
+            "UnityEngine.UI.VerticalLayoutGroup",
+            "UnityEngine.UI.BaseMeshEffect",
+            "UnityEngine.UI.Outline",
+            "UnityEngine.UI.PositionAsUV1",
+            "UnityEngine.UI.Shadow",
+            #endregion
+        };
+
+        public static readonly string[] ComponentTypeWhiteListSdk3 = {
+            "VRC.SDK3.VRCTestMarker",
+            "VRC.SDK3.Components.VRCPickup",
+            "VRC.SDK3.Components.VRCObjectSync",
+            "VRC.SDK3.Components.VRCStation",
+            "VRC.SDK3.Components.VRCSpatialAudioSource",
+            "VRC.SDK3.Props.Components.VRCPropDescriptor",
+            "VRC.Udon.UdonBehaviour",
+            "VRC.Udon.AbstractUdonBehaviourEventProxy",
+            "VRC.SDK3.Components.VRCUiShape",
+
+            // TODO: Props and Worlds don't initialize Dynamics properly yet.
+            // "VRC.SDK3.Dynamics.PhysBone.Components.VRCPhysBone",
+            // "VRC.SDK3.Dynamics.PhysBone.Components.VRCPhysBoneCollider",
+            // "VRC.SDK3.Dynamics.Constraint.Components.VRCAimConstraint",
+            // "VRC.SDK3.Dynamics.Constraint.Components.VRCLookAtConstraint",
+            // "VRC.SDK3.Dynamics.Constraint.Components.VRCParentConstraint",
+            // "VRC.SDK3.Dynamics.Constraint.Components.VRCPositionConstraint",
+            // "VRC.SDK3.Dynamics.Constraint.Components.VRCRotationConstraint",
+            // "VRC.SDK3.Dynamics.Constraint.Components.VRCScaleConstraint",
+            // "VRC.SDK3.Dynamics.Contact.Components.VRCContactSender",
+            // "VRC.SDK3.Dynamics.Contact.Components.VRCContactReceiver",
+        };
+    }
+}
+#endif
